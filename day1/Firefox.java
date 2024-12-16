83b372fcf1d10da4c0ea1734b6c3b5e559812426
komaldeshpande13 <76818003+komaldeshpande13@users.noreply.github.com>
2024-12-16 22:12:59 +0530

    Add files via upload
    Day1

---
+++ day1/Firefox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		
+	float	browserVersion =100.2f;
+	String  browserName ="firefox";
+	boolean isVisible =true;
+	int releaseYear =1998;
+	char browserLogo  ='f';
+	
+	System.out.println("Browser Name:"+browserName);
+	System.out.println("Browser Version:"+browserVersion +"\n"+
+	"Release Year:"+releaseYear);
+	System.out.println("Browser Logo:"+browserLogo +"\n" + 
+	"Is Browser Visible:"+isVisible);
+	}
+
+}
---
+++ day1/FirstProgram.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class FirstProgram {
+	
+	int num=2;
+	
+public static void main(String[] args) {
+	
+	int var =4;
+	char alpha='k';
+	boolean appLoaded=true;
+	float browserVersion=4.55555f; //f or F
+	double 	ap=3.888888888888888888888;
+	long phNo=7678767876l; //l or L
+	String name="komal";
+	
+	System.out.println("Welcome to Testleaf");
+}
+}
